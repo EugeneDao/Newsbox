e3f9c8fbb47cbe67ba2b56676db84c763fc7486a
Đặng Trần Phương Ngân <dangtranphuongngan@angs-MacBook-Pro.local>
2022-08-13 18:28:56 +0700

    final commit

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
-let url =
-  "https://newsapi.org/v2/top-headlines?country=us&apiKey=277f8ff1c93f4d7c98cbb220e9fc9d5b";
-async function getApi(url) {
-  const response = await fetch(url);
-  let data = await response.json();
-  let headlines = data.articles;
-  headlines.forEach(element => {
-    
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
   }
-  console.log(headlines[0]);
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 2000); // Change image every 2 seconds
 }
-getApi(url);
-
-//       const data = await conn.json();
-//       readAPI(json);
-//       function readAPI(data) {
-// const app = document.getElementById('app');
-// const h1 = document.createElement('h1');
-// h1.innerHTML = data.title;
-//       }
-
 function myFunction() {
   var x = document.getElementById("navDemo");
   if (x.className.indexOf("w3-show") == -1) {
